a3a19ae328a9aaf7eab40e8cea7683c3e009b888
Jack Casey <jackdcasey@gmail.com>
2020-05-28 20:19:33 -0700

    Scaffolding

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+import App from './App.vue'
+import './assets/css/skeleton/2.0.4/skeleton.css'
+import './assets/css/skeleton/2.0.4/normalize.css'
+import router from './router'
+
+Vue.use(VueRouter)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
